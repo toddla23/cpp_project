ba08c68ecf626de182e0531c0186e17e369ac947
Park Jonghyeok <84115021+toddla23@users.noreply.github.com>
2022-05-18 18:39:34 +0900

    [Fix}
    모서리 2
    벽 1
    필드 0
    뱀은 클래스로 만들듯...?

--- SnakeGame.cpp
+++ SnakeGame.cpp
@@ -7,19 +7,22 @@ int main()
 
     setlocale(LC_ALL, "");
 
+    int mapsize = 21;
+
     WINDOW * snake_win;
     initscr();
-    resize_term(25,60);
+    resize_term(25,40);
     start_color();
 
     border('|', '|', '-', '-', '+', '+', '+', '+');
     mvprintw(1, 6, "SNAKE GAME!");
     refresh();
 
-    snake_win = newwin(20,34,2,2);    
-    for(int i = 0; i < 18; i++)
-        for(int j = 1; j < 17; j++)
-            mvwprintw(snake_win, i, j, "\u2B1c");
+    snake_win = newwin(mapsize,mapsize,2,2); 
+    wborder(snake_win, '1', '1', '1', '1', '2', '2', '2', '2');   
+    for(int i = 1; i < mapsize-1; i++)
+        for(int j = 1; j < mapsize-1; j++)
+            mvwprintw(snake_win, i, j, "0");
     wrefresh(snake_win); 
 
 
